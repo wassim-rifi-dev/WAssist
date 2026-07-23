ae82343221019ed335729faa453f3e9970ab182c
wassimrifi <wassimrifi008@gmail.com>
2026-07-24 00:09:31 +0100

    feat: create ChatResponse class for chat response handling

---
+++ backend/src/main/java/dev/wassim/wassist/features/chat/dto/response/ChatResponse.java
@@ -0,0 +1,10 @@
+package dev.wassim.wassist.features.chat.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ChatResponse {
+    private String response;
+}
