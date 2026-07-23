02c4d7c28adef9a3467148abd989c38cf2267e6e
wassimrifi <wassimrifi008@gmail.com>
2026-07-23 23:51:40 +0100

    first commit

---
+++ backend/src/main/java/dev/wassim/wassist/WassistApplication.java
@@ -0,0 +1,13 @@
+package dev.wassim.wassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WassistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WassistApplication.class, args);
+	}
+
+}
