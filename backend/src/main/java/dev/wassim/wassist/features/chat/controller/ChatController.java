ec678239428c8abad4df5834979b9fd3cdf60f12
wassimrifi <wassimrifi008@gmail.com>
2026-07-24 00:37:53 +0100

    feat: add ChatController for handling chat requests

---
+++ backend/src/main/java/dev/wassim/wassist/features/chat/controller/ChatController.java
@@ -0,0 +1,11 @@
+package dev.wassim.wassist.features.chat.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+
+@RestController
+@AllArgsConstructor
+public class ChatController {
+    
+}
