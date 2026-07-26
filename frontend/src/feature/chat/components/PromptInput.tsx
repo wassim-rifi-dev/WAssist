cd8f034d485e313148b7b9af0d2bdcc032efbe3d
wassimrifi <wassimrifi008@gmail.com>
2026-07-26 20:24:04 +0100

    feat: enhance PromptInput and SubmitButton to manage thinking state during prompt submission

--- frontend/src/feature/chat/components/PromptInput.tsx
+++ frontend/src/feature/chat/components/PromptInput.tsx
@@ -6,16 +6,17 @@ type Props = {
     handleSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
     handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
     prompt: string;
+    thinking: boolean;
 };
 
-export default function PromptInput({ handleSubmit , handleChange , prompt }: Props) {
+export default function PromptInput({ handleSubmit , handleChange , prompt , thinking }: Props) {
     return (
         <form
             className="flex gap-10 bg-[#1D2027] p-5 rounded-2xl"
             onSubmit={ handleSubmit }
         >
             <Input handleChange={handleChange} prompt={prompt} />
-            <SubmitButton />
+            <SubmitButton thinking={thinking} prompt={prompt} />
         </form>
     )
 }
