04a866ceed53996b3e1fa6981c8d181f8d3401e5
wassimrifi <wassimrifi008@gmail.com>
2026-07-26 20:52:20 +0100

    fix: adjust layout of PromptInput form for better alignment

--- frontend/src/feature/chat/components/PromptInput.tsx
+++ frontend/src/feature/chat/components/PromptInput.tsx
@@ -12,7 +12,7 @@ type Props = {
 export default function PromptInput({ handleSubmit , handleChange , prompt , thinking }: Props) {
     return (
         <form
-            className="flex gap-10 bg-[#1D2027] p-5 rounded-2xl"
+            className="flex justify-between gap-10 bg-[#1D2027] p-5 rounded-2xl"
             onSubmit={ handleSubmit }
         >
             <Input handleChange={handleChange} prompt={prompt} />
